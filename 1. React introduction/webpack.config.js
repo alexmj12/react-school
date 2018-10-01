f9ac26c94fd783d4be3d55f5655d9ebda659f361
Alexander Mandrik <alexander.mandrik01@office.ctco.lv>
2018-10-01 15:12:15 +0300

    Add external build

--- 1. React introduction/webpack.config.js
+++ 1. React introduction/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackExternalsPlugin = require("html-webpack-externals-plugin");
 
 module.exports = {
   // Changed the entry point to a .tsx file
@@ -18,22 +19,33 @@ module.exports = {
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      { 
-        test: /\.tsx?$/, 
-        loader: "awesome-typescript-loader" 
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
       }
     ]
   },
-  // externals: {
-  //   "react": "React",
-  //   "react-dom": "ReactDOM"
-  // },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  },
   plugins: [
     new HtmlWebpackPlugin({
-        inject: true,
-        hash: true,
-        template: "./src/index.html",
-        filename: "index.html"
-      })
+      inject: true,
+      hash: true,
+      template: "./src/index.html",
+      filename: "index.html"
+    }),
+    new HtmlWebpackExternalsPlugin({
+      externals: [{
+        module: "react",
+        entry: "umd/react.development.js",
+        global: "React",
+      }, {
+        module: "react-dom",
+        entry: "umd/react-dom.development.js",
+        global: "ReactDOM",
+      }]
+    })
   ]
 }

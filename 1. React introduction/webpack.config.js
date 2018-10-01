5f26de9a447e32723ded484ffa1e871674b5760f
Alexander Mandrik <alexander.mandrik01@office.ctco.lv>
2018-10-01 14:18:04 +0300

    Add basic project

---
+++ 1. React introduction/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  // Changed the entry point to a .tsx file
+  entry: "./src/index.tsx",
+  // Enable sourcemaps for debugging Webpack's output
+  devtool: "source-map",
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.js"
+  },
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { 
+        test: /\.tsx?$/, 
+        loader: "awesome-typescript-loader" 
+      }
+    ]
+  },
+  // externals: {
+  //   "react": "React",
+  //   "react-dom": "ReactDOM"
+  // },
+  plugins: [
+    new HtmlWebpackPlugin({
+        inject: true,
+        hash: true,
+        template: "./src/index.html",
+        filename: "index.html"
+      })
+  ]
+}
